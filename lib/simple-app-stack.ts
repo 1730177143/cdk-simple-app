99b92761003da8b47b30321d54cc03dcba7dda36
Long Liu <1730177143@qq.com>
2024-02-24 01:01:14 +0000

    Lambda communicating with DynamoDB

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -4,7 +4,7 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import {Construct} from 'constructs';
 import * as custom from "aws-cdk-lib/custom-resources";
-import { generateBatch } from "../shared/util";
+import {generateBatch} from "../shared/util";
 import {movies} from "../seed/movies";
 
 export class SimpleAppStack extends cdk.Stack {
@@ -26,7 +26,7 @@ export class SimpleAppStack extends cdk.Stack {
         });
         const moviesTable = new dynamodb.Table(this, "MoviesTable", {
             billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-            partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
+            partitionKey: {name: "id", type: dynamodb.AttributeType.NUMBER},
             removalPolicy: cdk.RemovalPolicy.DESTROY,
             tableName: "Movies",
         });
@@ -45,6 +45,32 @@ export class SimpleAppStack extends cdk.Stack {
                 resources: [moviesTable.tableArn],
             }),
         });
+        const getMovieByIdFn = new lambdanode.NodejsFunction(
+            this,
+            "GetMovieByIdFn",
+            {
+                architecture: lambda.Architecture.ARM_64,
+                runtime: lambda.Runtime.NODEJS_16_X,
+                entry: `${__dirname}/../lambdas/getMovieById.ts`,
+                timeout: cdk.Duration.seconds(10),
+                memorySize: 128,
+                environment: {
+                    TABLE_NAME: moviesTable.tableName,
+                    REGION: 'eu-west-1',
+                },
+            }
+        );
+
+        const getMovieByIdURL = getMovieByIdFn.addFunctionUrl({
+            authType: lambda.FunctionUrlAuthType.NONE,
+            cors: {
+                allowedOrigins: ["*"],
+            },
+        });
+
+        moviesTable.grantReadData(getMovieByIdFn)
+
+        new cdk.CfnOutput(this, "Get Movie Function Url", {value: getMovieByIdURL.url});
         new cdk.CfnOutput(this, "Simple Function Url", {value: simpleFnURL.url});
     }
 }
